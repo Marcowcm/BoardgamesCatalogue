beddb8607e7cefcd059c59f8b00a946f8601e359
Marcowcm <marcowcm@gmail.com>
2024-02-04 03:04:57 +0800

    jsonIMGfixed

--- static/js/index.js
+++ static/js/index.js
@@ -8,17 +8,14 @@ $(document).ready(function(){
         games = data;
         renderGames(games);
     }})
-    renderGames(games);
 })
 
 function renderGames(data){
-    console.log(data);
-    console.log(games);
     for (var i = 0; i < data.length; i++) {
             var html = `<div class="col s12 m6 l4"> 
                         <div class="card horizontal"> 
                             <div class="card-image waves-effect waves-block waves-light">
-                                <img class="activator" src="static/${(data[i].IMG="")?"IMG/default.png":data[i].IMG}"> 
+                                <img class="activator" src="static/${( data[i].IMG= "")?"IMG/default.png":data[i].IMG}"> 
                             </div>
                             <div class="">
                                 <div class="card-content">
@@ -32,14 +29,13 @@ function renderGames(data){
                                     <p class="time">Est:${data[i].Est_Duration}</p>
                                 </div>
                             </div>
-                            <div class="card-reveal">'
+                            <div class="card-reveal">
                                 <span class="card-title grey-text text-darken-4">${data[i].Name_eng}<i class="material-icons right">close</i></span>
                                 <p>${data[i].Description}</p>
-                                {}<a href="${data[i].BGG}">See on BGG</a>
+                                <a href="${data[i].BGG}">See on BGG</a>
                             </div>
                         </div>
                         </div>`
             $('#catelogue').append(html);
-            console.log(i)
     }
 }
