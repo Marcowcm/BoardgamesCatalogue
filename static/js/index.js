52263c0a3908bccad0c202a8ff1ae397bd916503
Marcowcm <marcowcm@gmail.com>
2024-02-04 02:44:36 +0800

    TestImageRender

--- static/js/index.js
+++ static/js/index.js
@@ -1,20 +1,27 @@
-
-$(document).ready(function(){   
+var games   
+$(document).ready(function(){
     $.ajax({ 
     type: 'GET', 
     url: 'static/data.json', 
     dataType: 'json',
     success: function (data) { 
-        for (var i = 0; i < data.length; i++) {
+        games = data  
+    }})
+    renderGames(games)
+})
+
+function renderGames(data){
+    console.log("it works")
+    for (var i = 0; i < data.length; i++) {
             var html = `<div class="col s12 m6 l4"> 
                         <div class="card horizontal"> 
                             <div class="card-image waves-effect waves-block waves-light">
-                                <img class="activator" src="static/${(data[i].IMG="")?"IMG/default.jpeg":data[i].IMG}"> 
+                                <img class="activator" src="static/${(data[i].IMG="")?"IMG/default.png":data[i].IMG}"> 
                             </div>
                             <div class="">
                                 <div class="card-content">
                                     <span class="activator card-title">
-                                        ${(data[i].Name_eng != "")?data[i].Name_eng :html += data[i].Name_zh}
+                                        ${(data[i].Name_eng != "")?data[i].Name_eng :data[i].Name_zh}
                                         <i class="material-icons right blue-text text-darken-3">more_vert</i>
                                     </span>
                                     <p class="players" min="${data[i].players_min}" max="${data[i].players_max}">
@@ -32,7 +39,5 @@ $(document).ready(function(){
                         </div>`
             $('#catelogue').append(html);
             console.log(i)
-            
-        }
-}})
-})
+    }
+}
